cdfb67a0a18f4b130545f54e218cb2e9e4a16126
Javul Mulla <javulmulla8@gmail.com>
2022-08-02 15:36:23 +0530

    [javul]logical program added to remote repo

---
+++ src/logicalpgm/ReverseNum.java
@@ -0,0 +1,30 @@
+package logicalpgm;
+
+	import java.util.Scanner; 
+
+	public class ReverseNum {
+
+	    public static void main(String[] args) {
+
+	        Scanner sc = new Scanner(System.in);  
+
+	        int num , reversed = 0;
+
+	        System.out.print("Enter number to reverse it : ");
+
+	        num = sc.nextInt();
+
+	        while(num != 0) {
+
+	            int digit = num % 10;
+
+	            reversed = reversed * 10 + digit;
+	            
+	            num /= 10;
+
+	        }
+
+	        System.out.println("Reversed Number: " + reversed);
+	    }
+
+}
